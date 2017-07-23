b2199cb54114d0ba988d7db4b38f60967b26a210
FarisHamdi <firastn@gmail.com>
2017-07-23 15:35:14 -0700

    main_menu added

---
+++ snippets/README.md
@@ -0,0 +1,2 @@
+All the cpp files in this directory need to be recompiled using g++ when imported or cloned.
+These snippets will be used in the ros build of the kennedy prototype. 
---
+++ snippets/main_menu.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void callmenu() {
+	cout << "--------------------Kennedy Main Menu--------------------\n";
+	cout << "A. 				B.					C.\n";
+	cout << "I. IMU				B.					C.\n";
+	cout << "T. Thermal			C.					V. version\n";
+	cout << "--------------------Underconstruction--------------------\n"	;
+	}
+
+int main() {
+
+	string userinput;
+
+	callmenu();
+	cin >> userinput;
+
+return 0;
+}
